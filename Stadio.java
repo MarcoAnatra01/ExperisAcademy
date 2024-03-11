0471c42975a885fc19321d50bddbadaf1cde56c9
MarcoAnatra01 <133258225+MarcoAnatra01@users.noreply.github.com>
2024-03-11 17:35:34 +0100

    commit finale

--- Stadio.java
+++ Stadio.java
@@ -6,35 +6,51 @@ public class Stadio {
     
     public static void main(String[] args) {
         int spectatorID = 0;
-        int incassiTotali = 0;
+        int incassoTotale = 0;
         int prezzoSabato = 20;
         int prezzoDomenica = 15;
         
         Scanner scanNum = new Scanner(System.in);
         Random random = new Random();
-
+        ArrayList<Integer> spettatoriSabato = new ArrayList<>();
+        ArrayList<Integer> spettatoriDomenica = new ArrayList<>();
         
         System.out.println("Inserisci il numero di weekend:\n");
         int weekend = scanNum.nextInt();
         
         if (weekend > 0) {
+
+            // vendita biglietti sabato
             int i = 0;
             while (i < weekend) {
-                // simulazione vendita biglietti sabato
-                int spettatoriSabato = random.nextInt(30000);
-                int incassoSabato = spettatoriSabato * prezzoSabato;
-                spectatorID += spettatoriSabato;
-    
-                // simulazione vendita biglietti domenica
-                int spettatoriDomenica = random.nextInt(30000);
-                int incassoDomenica = spettatoriDomenica * prezzoDomenica;
-                spectatorID += spettatoriDomenica;
-                
-                incassiTotali += (incassoSabato + incassoDomenica);
+                int spettatori = random.nextInt(30000);  // generazione casuale numero spettatori
+                spettatoriSabato.add(spettatori);
+                int incasso = spettatori * prezzoSabato;
+                incassoTotale += incasso;
                 i++;
+            }
+
+            // vendita biglietti domenica
+            int j = 0;
+            while (j < weekend) {
+                int spettatori = random.nextInt(30000);  // generazione casuale numero spettatori
+                spettatoriDomenica.add(spettatori);
+                int incasso = spettatori * prezzoDomenica;
+                incassoTotale += incasso;
+                j++;
             }
         }else{
             System.out.println("\nhai inserito 0 weekend");
         }
+
+/*
+        for (int i = 0; i < spettatoriSabato.size(); i++) {
+            System.out.println("Sabato " + (i+1) + ": Spettatori = " + spettatoriSabato.get(i) + ", Incasso = $" + (saturdaySpectators.get(i) * 20));
+        }
+        
+        for (int i = 0; i < sundaySpectators.size(); i++) {
+            System.out.println("Domenica " + (i+1) + ": Spettatori = " + sundaySpectators.get(i) + ", Incasso = $" + (sundaySpectators.get(i) * 15));
+        }
+        */
     }
 }
